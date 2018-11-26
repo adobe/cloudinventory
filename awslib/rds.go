185978e48e0bda74cd92641594144363656bcbae
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2018-11-26 16:29:59 +0530

    Added backoff to RDS as well

--- awslib/rds.go
+++ awslib/rds.go
@@ -1,6 +1,9 @@
 package awslib
 
 import (
+	"time"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/jpillora/backoff"
 	"errors"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds"
@@ -12,9 +15,24 @@ func GetAllDBInstances(sess *session.Session) ([]*rds.DBInstance, error) {
 	allInstancesDone := false
 	var allInstances []*rds.DBInstance
 	input := rds.DescribeDBInstancesInput{}
+	b := &backoff.Backoff{
+		//These are the defaults
+		Min:    10 * time.Millisecond,
+		Max:    30 * time.Second,
+		Factor: 2,
+		Jitter: false,
+	}
 	for !allInstancesDone {
 		// Describe instances with no filters
 		result, err := rdsc.DescribeDBInstances(&input)
+		if err != nil {
+			if _, ok := err.(awserr.Error); ok {
+				// Retry with backoff
+				time.Sleep(b.Duration())
+				continue
+			}
+			return allInstances, err
+		}
 		if err != nil {
 			return nil, errors.New("Error Describing Instances")
 		}
