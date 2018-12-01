1f9948966bbd1cd852434ea2e7abb5f3f3775534
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2018-12-01 14:05:42 +0530

    Added RDS test

--- awslib/aws_test.go
+++ awslib/aws_test.go
@@ -8,7 +8,7 @@ import (
 // This test REQUIRES a working AWS account and credentials to read from EC2
 // Since the validity of the test depends on the account itself, this test is written to be as generic as possible
 // This test does NOT fail unless there is an error in the gathering, the gathering itself is not validated.TestGetAllInstances
-// Use --verbose to see instance numbers
+// Use verbose testing to see instance numbers
 func TestGetAllInstances(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping test in short mode")
@@ -23,5 +23,27 @@ func TestGetAllInstances(t *testing.T) {
 			t.Errorf("Failed to get Instances for region: %s because %v", r, err)
 		}
 		t.Logf("Found %d instances in %s", len(instances), r)
+	}
+}
+
+// TestGetAllDBInstances checks if the lib is able to gather all instances properly or not.
+// This test REQUIRES a working AWS account and credentials to read from RDS
+// Since the validity of the test depends on the account itself, this test is written to be as generic as possible
+// This test does NOT fail unless there is an error in the gathering, the gathering itself is not validated.TestGetAllInstances
+// Use verbose testing to see instance numbers
+func TestGetAllDBInstances(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping test in short mode")
+	}
+	sessions, err := BuildSessions(GetAllRegions())
+	if err != nil {
+		t.Errorf("Unable to get sessions : %v", err)
+	}
+	for r, sess := range sessions {
+		dbinstances, err := GetAllDBInstances(sess)
+		if err != nil {
+			t.Errorf("Failed to get Instances for region: %s because %v", r, err)
+		}
+		t.Logf("Found %d instances in %s", len(dbinstances), r)
 	}
 }
