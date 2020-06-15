9dd08d87bcfb2cc403f309c837423f54ecc80644
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-06-15 09:22:25 +0530

    updated base_test.go
    Added camel casing

--- azurelib/base_test.go
+++ azurelib/base_test.go
@@ -1,22 +1,21 @@
 package azurelib
 
 import (
-	"context"
-	"testing"
-	"time"
+        "context"
+        "testing"
+        "time"
 )
 
+// TestGetAllSubscriptionIDs tests function GetAllSubscriptionIDs
+func TestGetAllSubscriptionIDs(t *testing.T) {
 
-//TestGetallSubscriptionIDs tests function GetallSubscriptionIDs
-func TestGetallSubscriptionIDs(t *testing.T) {
+        ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+        defer cancel()
+        subscriptionMap, err := GetAllSubscriptionIDs(ctx)
+        if err != nil {
+                t.Errorf("Failed to  Get all SubscriptionIDs: %v", err)
+        } else {
+                t.Logf("GetallSubscriptionIDs successful.Found %d subscription IDs", len(subscriptionMap))
 
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	defer cancel()
-	subscriptionmap,err := GetallSubscriptionIDs(ctx)
-	if err != nil {
-			t.Errorf("Failed to  Get all SubscriptionIDs: %v", err)
-	} else {
-			t.Logf("GetallSubscriptionIDs successful.Found %d subscription IDs",len(subscriptionmap))
-			
-	}
+        }
 }
