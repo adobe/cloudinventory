8d52f5d7ea9be1b175db5b396f914d5a1cdf5925
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-06-15 09:21:00 +0530

    updated base.go
    Added camel casing

--- azurelib/base.go
+++ azurelib/base.go
@@ -6,26 +6,26 @@ import (
         "github.com/Azure/go-autorest/autorest/azure/auth"
 )
 
-//GetallSubscriptionIDs function returns a map of subscription name and subscription ID at account level
-func GetallSubscriptionIDs(ctx context.Context) (map[string]string, error) {
-        subscriptionmap := make(map[string]string)
+// GetAllSubscriptionIDs function returns a map of subscription name and subscription ID at account level
+func GetAllSubscriptionIDs(ctx context.Context) (map[string]string, error) {
+        subscriptionMap := make(map[string]string)
         authorizer, err := auth.NewAuthorizerFromEnvironment()
         if err != nil {
-                return subscriptionmap, err
+                return subscriptionMap, err
         }
         newClient := subscriptions.NewClient()
         newClient.Authorizer = authorizer
         result, err := newClient.ListComplete(ctx)
         if err != nil {
-                return subscriptionmap, err
+                return subscriptionMap, err
         }
         for result.NotDone() {
                 subscription := result.Value()
-                subscriptionmap[*subscription.DisplayName] = *subscription.SubscriptionID
+                subscriptionMap[*subscription.DisplayName] = *subscription.SubscriptionID
                 if err := result.Next(); err != nil {
-                        return subscriptionmap, err
+                        return subscriptionMap, err
                 }
         }
-        return subscriptionmap, nil
+        return subscriptionMap, nil
 
 }
