95f7df2542f02ac47b7adaaac6bc81b97a568cd2
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-06-25 11:05:57 +0530

    Added base.go file
    Has function GetAllSubscriptionIDs which returns a map of subscription name and subscription ID at account level

---
+++ azurenetwork/base.go
@@ -0,0 +1,31 @@
+package azurelib
+
+import (
+        "context"
+        "github.com/Azure/azure-sdk-for-go/profiles/2019-03-01/resources/mgmt/subscriptions"
+        "github.com/Azure/go-autorest/autorest/azure/auth"
+)
+
+// GetAllSubscriptionIDs function returns a map of subscription name and subscription ID at account level
+func GetAllSubscriptionIDs(ctx context.Context) (map[string]string, error) {
+        subscriptionMap := make(map[string]string)
+        authorizer, err := auth.NewAuthorizerFromEnvironment()
+        if err != nil {
+                return subscriptionMap, err
+        }
+        newClient := subscriptions.NewClient()
+        newClient.Authorizer = authorizer
+        result, err := newClient.ListComplete(ctx)
+        if err != nil {
+                return subscriptionMap, err
+        }
+        for result.NotDone() {
+                subscription := result.Value()
+                subscriptionMap[*subscription.DisplayName] = *subscription.SubscriptionID
+                if err := result.Next(); err != nil {
+                        return subscriptionMap, err
+                }
+        }
+        return subscriptionMap, nil
+
+}
