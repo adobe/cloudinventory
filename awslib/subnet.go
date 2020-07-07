11ce840aa41216fdeac8018c2036092055abebca
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-07-07 14:23:59 +0530

    Added changes to GetALLSubnetInstances()
    Added changes to GetALLSubnetInstances() according to aws-sdk-go v1.19.16

--- awslib/subnet.go
+++ awslib/subnet.go
@@ -13,53 +13,20 @@ governing permissions and limitations under the License.
 package awslib
 
 import (
-	"time"
-
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/jpillora/backoff"
 )
 
-// GetAllSubnetInstances returns a complete list of instances for a given session
+// GetAllSubnetInstances returns a complete list of instances for a given session 
+// Needs to get updated with the latest aws-sdk-go version
 func GetAllSubnetInstances(sess *session.Session) ([]*ec2.Subnet, error) {
 	ec2c := ec2.New(sess)
-	allInstancesDone := false
 	var allInstances []*ec2.Subnet
 	input := ec2.DescribeSubnetsInput{}
-	b := &backoff.Backoff{
-		//These are the defaults
-		Min:    10 * time.Millisecond,
-		Max:    30 * time.Second,
-		Factor: 2,
-		Jitter: false,
+	result, err := ec2c.DescribeSubnets(&input)
+	if err != nil {
+		return allInstances, err
 	}
-	for !allInstancesDone {
-		// Describe instances with no filters
-		result, err := ec2c.DescribeSubnets(&input)
-		if err != nil {
-			if aerr, ok := err.(awserr.Error); ok {
-				// Retry with backoff if RateExceeded
-				if aerr.Code() == "RateExceeded" {
-					time.Sleep(b.Duration())
-					continue
-				}
-
-				return allInstances, aerr
-
-			}
-
-			return allInstances, err
-		}
-		b.Reset()
-		for _, reservation := range result.Subnets  {
-			allInstances = append(allInstances, reservation)
-		}
-		if result.NextToken == nil {
-			allInstancesDone = true
-			continue
-		}
-		input.SetNextToken(*result.NextToken)
-	}
+	allInstances = result.Subnets	
 	return allInstances, nil
 }
