ac0bd305727e078832c495096721fbc7e7f41444
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-07-22 13:19:18 +0530

    Added TestGetAllCDN function
    TestGetAllCDN tests the function GetAllCDN

--- azurelib/azure_test.go
+++ azurelib/azure_test.go
@@ -79,3 +79,23 @@ func TestGetAllLdb(t *testing.T) {
                 t.Logf("Found %d loadbalancers in %s", len(Ldb), key)
         }
 }
+
+// TestGetAllCDN tests the function GetAllCDN
+func TestGetAllCDN(t *testing.T) {
+        if testing.Short() {
+                t.Skip("Skipping test in short mode")
+        }
+        ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+        defer cancel()
+        subscriptions, err := GetAllSubscriptionIDs(ctx)
+        if err != nil {
+                t.Errorf("Unable to get subscriptionIDs: %v", err)
+        }
+        for key, subsID := range subscriptions {
+                Ldb, err := GetAllCDN(subsID)
+                if err != nil {
+                        t.Errorf("Failed to get CDN for subscription: %s because %v", key, err)
+                }
+                t.Logf("Found %d CDN in %s", len(Ldb), key)
+        }
+}
