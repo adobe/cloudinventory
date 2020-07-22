fa103c228a85170613b2b2de6d514d6bb284316a
Thushara67 <51262047+Thushara67@users.noreply.github.com>
2020-07-22 13:20:40 +0530

    Added TestCollectCDN function
    TestCollectCDN test the function CollectCDN

--- azurecollector/azurecollect_test.go
+++ azurecollector/azurecollect_test.go
@@ -49,3 +49,19 @@ func TestCollectLoadBalancers(t *testing.T) {
                 t.Errorf("Failed to collect Load Balancers: %v", err)
         }
 }
+
+// TestCollectCDN test the function CollectCDN
+func TestCollectCDN(t *testing.T) {
+        if testing.Short() {
+                t.Skip("Skipping test in short mode")
+        }
+        col, err := NewAzureCollector()
+        if err != nil {
+                t.Errorf("Failed to create collector: %v", err)
+        }
+        maxGoRoutines := len(col.SubscriptionMap)
+        _, _, err = col.CollectCDN(maxGoRoutines)
+        if err != nil {
+                t.Errorf("Failed to collect CDN: %v", err)
+        }
+}
